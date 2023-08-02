96271f16a3eb38d7db9e17195d6073035f7422e4
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-08-02 21:10:03 +0530

    first commit

---
+++ Subject.cs
@@ -0,0 +1,7 @@
+﻿namespace Phase1Section1._9
+{
+    internal class Subject
+    {
+        public string Name { get; internal set; }
+    }
+}
---
+++ SubjectTeacher.cs
@@ -0,0 +1,18 @@
+﻿using Phase1Section1._9;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phase1Section1._9
+{
+    public class SubjectTeacher : Teacher
+    {
+        private Subject whichSubject;
+
+        public Subject WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+        }
+    }
+}
